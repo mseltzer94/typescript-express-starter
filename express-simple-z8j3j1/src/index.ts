917f235e80072473377bb6af4f31985b4ca23799
Michael Seltzer <michael.m.seltzer@gmail.com>
2023-02-23 13:24:54 -0500

    Delete express-simple-z8j3j1 directory

--- express-simple-z8j3j1/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { createServer } from './server';
-
-const app = createServer();
-const port = 3100;
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
